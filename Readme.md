dc3ce2fa81d923b9818aee79b4f00875247e6d04
emanuele-f <black.silver@hotmail.it>
2015-06-14 02:43:44 +0200

    update Readme and add gpl notice

--- Readme.md
+++ Readme.md
@@ -1,22 +1,29 @@
-DEPS
-----
+#Fuzzy Project
 
-- cmake
+Allegro 5 sperimentation, server-client comunication, 2D tiled world...with
+a possible strategic game future in mind.
+
+See src/network module for a minimal implementation of a TCP endianess/bitness
+independent network library.
+
+See src/fuzzy for a set of utility functions.
+
+Dependencies
+------------
 - allegro5
-- allegro_tiled: xml2 zlib glib2.0
+- tmx: cmake zlib libxml2
 - libuuid
 
-OPTIONAL TOOLS
---------------
+Optional deps
+-------------
+- tiled - a tiled map editor: qmake qt4
 
-- tiled: qmake qt4
-
-
-RULES
------
+Make rules
+----------
 
 - make init: pulls in the dependencies and tools
 - make: build the game
 - make debug: enables debug info. requires a 'clean' to rebuild
+- make tools: builds external tools, like the Tiled map editor
 - make clean: removes any built binary
 - make cleanall: removes any built binary, any dependency and any tool
--- src/network.h
+++ src/network.h
@@ -1,9 +1,21 @@
-/**
-    \file network.h
-    \author Emanuele Faranda
+/*
+ * Emanuele Faranda         black.silver@hotmail.it
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-    13-Jun-2015
-
+/*
     A library to abstract network/calculator subsystem to perform network
     operations.
 
@@ -15,8 +27,6 @@
 
     Ordering: network MSB first, network LSB after
     Size: size is encoded in 32bit storage
-
-    _msglen: only used to send length
 
     \note cursor always points to a new insertion byte
  */
